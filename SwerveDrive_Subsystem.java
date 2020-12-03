3a1ba5ce77c504732a580100ea6155eadcae4ced
WilliamChen1109 <49446811+WilliamChen1109@users.noreply.github.com>
2020-12-03 13:07:14 +0800

    Update SwerveDrive_Subsystem.java

--- SwerveDrive_Subsystem.java
+++ SwerveDrive_Subsystem.java
@@ -17,3 +17,56 @@ public void drive (double x1, double y1, double x2) {
     double frontRightAngle = Math.atan2 (b, d) / Math.pi;
     double frontLeftAngle = Math.atan2 (b, c) / Math.pi;
 }
+
+private Jaguar angleMotor;
+private Jaguar speedMotor;
+private PIDController pidController;
+
+public WheelDrive (int angleMotor, int speedMotor, int encoder) {
+    this.angleMotor = new Jaguar (angleMotor);
+    this.speedMotor = new Jaguar (speedMotor);
+    pidController = new PIDController (1, 0, 0, new AnalogInput (encoder), this.angleMotor);
+
+    pidController.setOutputRange (-1, 1);
+    pidController.setContinuous ();
+    pidController.enable ();
+}
+
+private final double MAX_VOLTS = 4.95;
+
+public void drive (double speed, double angle) {
+    speedMotor.set (speed);
+
+    double setpoint = angle * (MAX_VOLTS * 0.5) + (MAX_VOLTS * 0.5); // Optimization offset can be calculated here.
+    if (setpoint < 0) {
+        setpoint = MAX_VOLTS + setpoint;
+    }
+    if (setpoint > MAX_VOLTS) {
+        setpoint = setpoint - MAX_VOLTS;
+    }
+
+    pidController.setSetpoint (setpoint);
+}
+
+private WheelDrive backRight;
+private WheelDrive backLeft;
+private WheelDrive frontRight;
+private WheelDrive frontLeft;
+
+public SwerveDrive (WheelDrive backRight, WheelDrive backLeft, WheelDrive frontRight, WheelDrive frontLeft) {
+    this.backRight = backRight;
+    this.backLeft = backLeft;
+    this.frontRight = frontRight;
+    this.frontLeft = frontLeft;
+}
+
+    backRight.drive (backRightSpeed, backRightAngle);
+    backLeft.drive (backLeftSpeed, backLeftAngle);
+    frontRight.drive (frontRightSpeed, frontRightAngle);
+    frontLeft.drive (frontLeftSpeed, frontLeftAngle);
+
+private Joystick joystick = new Joystick (0);
+
+public void teleopPeriodic () {
+    swerveDrive.drive (joystick.getRawAxis (1), joystick.getRawAxis (0), joystick.getRawAxis (4));
+}
